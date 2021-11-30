653ec4ce59a6c778b28e1106e05df221fee03785
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-11-30 14:53:40 -0800

    modify cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'https://eatsypeasy.netlify.app/'
+    origins 'localhost:3001', 'https://eatsypeasy.netlify.app/', '*'
 
     resource '*',
       headers: :any,
